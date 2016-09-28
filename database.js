93ded48d67a078a0c83da6e2a251a8ada766ad0c
Alberto Romero <lguitarras0594@gmail.com>
2016-09-28 10:48:12 -0500

    sets up the query to insert hash and original url

--- database.js
+++ database.js
@@ -1,9 +1,11 @@
 const connectionString = 'postgres://alberto:Setzer789&@localhost:5432/shortened_urls';
 const pgp = require('pg-promise')();
+const db = pgp(connectionString);
 
 const database = {
   insertHashAndOriginalURL: args => {
-
+    return db.oneOrNone('INSERT INTO short_url (hash, original_url, visits)' +
+      'values (${hash}, ${originalURL}, 0)', args);
   }
 };
 

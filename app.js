e09b2d0c07f8186f9bec8ef9d951b77cd5dbde70
Alberto Romero <lguitarras0594@gmail.com>
2016-09-28 10:29:23 -0500

    First commit. Set ups the basics for back and front end

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = 3000;
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', (req, res) => {
+  res.sendFile(buildRouteURL('index.html'));
+});
+
+app.post('/short', (req, res) => {
+
+});
+
+app.get('/admin', (req, res) => {
+
+});
+
+app.get('/*', (req, res) => {
+
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
+
+const buildRouteURL = s => `${__dirname}/${s}`;
+
+module.exports = app;
